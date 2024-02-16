920ea0d98acb1f4b8897aa118da7d3d73e3a39c4
Farmer.Chillax <48387781+FarmerChillax@users.noreply.github.com>
2024-02-17 00:12:51 +0800

    Update install.sh

--- mac/tools/install.sh
+++ mac/tools/install.sh
@@ -4,5 +4,6 @@ echo 🍁 开始安装 软件清单
 brew install --cask devtoys
 brew install --cask iterm2
 brew install --cask another-redis-desktop-manager
+brew install --cask dozer
 brew install maccy
 echo 🎉 安装完成
